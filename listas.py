5e1e44f48bda27ab6f382667878ba4b8b0fa6cfa
Raissa <raissan084@gmail.com>
2025-12-30 18:21:26 -0300

    add listas

---
+++ listas.py
@@ -0,0 +1,15 @@
+frutas = ["laranja", "uva", "melancia"]
+print(frutas[0]) #laranja
+print(frutas[-1]) #melancia
+letras = list("python")
+numeros = list(range(10))
+carro = ["Ferrari", "F8", 1998, 1.0]
+
+#listas aninhadas - lista dentro de listas
+matriz = [[1,2],
+          [1,3],
+          [2,4]]
+print(matriz)
+#fatiamento
+print(carro[:2])
+print(carro[::])
