613307020c463f3a5d6d590b607fc9283f7b6294
Raissa <raissan084@gmail.com>
2025-12-30 18:39:00 -0300

    add tuplas

---
+++ tupas.py
@@ -0,0 +1,11 @@
+# tuplas são IMUTÁVEIS!!!
+frutas = ("melancia","uva",) #virgula no final!
+print(frutas)
+print(frutas[0])
+letras = tuple("python")
+
+
+#tuplas aninhadas
+matriz =( (1,12),(12,1))
+
+#fatiamento possui a mesma lógica que as listas
