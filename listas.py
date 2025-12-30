3be69c96293d5b271b42679fa45a3785b9cb97fd
Raissa <raissan084@gmail.com>
2025-12-30 18:34:16 -0300

    add listas

--- listas.py
+++ listas.py
@@ -3,6 +3,8 @@ print(frutas[0]) #laranja
 print(frutas[-1]) #melancia
 letras = list("python")
 numeros = list(range(10))
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
 carro = ["Ferrari", "F8", 1998, 1.0]
 
 #listas aninhadas - lista dentro de listas
@@ -13,3 +15,10 @@ print(matriz)
 #fatiamento
 print(carro[:2])
 print(carro[::])
+
+#CLASSE LIST
+list = []
+list.append(10)
+print(list)
+list2 = list.copy()
+print(list2)
